2662338dbf46df8faa599bbd4a8a1d910cb6631e
ragavi272005 <230701249@rajalakshmi.edu.in>
2024-06-17 11:08:59 +0530

    Create week.2.3.py

---
+++ week.2.3.py
@@ -0,0 +1,5 @@
+num = int(input()) count = int(input())
+print(count % num == 0, end = " ") count = int(input())
+print(count % num == 0, end = " ") count = int(input())
+print(count % num == 0, end = " ") count = int(input())
+print(count % num == 0, end = " ")
