c9192f8b8905f0eb8ea6951900ca93751e5a60fc
ragavi272005 <230701249@rajalakshmi.edu.in>
2024-06-17 11:10:35 +0530

    Create week.2.5.py

---
+++ week.2.5.py
@@ -0,0 +1,7 @@
+basic = int(input())
+basic += basic * .04
+print(f"Balance as of end of Year 1: ${basic:.2f}.")
+basic += basic * .04
+print(f"Balance as of end of Year 2: ${basic:.2f}.")
+basic += basic * .04
+print(f"Balance as of end of Year 3: ${basic:.2f}.")
